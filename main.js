6899f523f0ec5946b14fc3ef4f0ef5ec423a2f1d
KaKi87 <KaKi87@proton.me>
2025-07-19 23:10:51 +0200

    :bug: Fix 94e2b81

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ for(const {
         continue;
     }
     console.log(`Downloading ${url}`);
-    await $`curl -L ${url} | tar xJ 2>/dev/null`;
+    await $`curl -L ${url} | tar xJ || true`;
     console.log(`Staging version ${version}`);
     await $`git add vivaldi-source`
     console.log(`Committing version ${version}`);
