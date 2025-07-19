94e2b815763924a1c983f58c0e5559759795524e
KaKi87 <KaKi87@proton.me>
2025-07-19 14:43:48 +0200

    :construction_worker: Add GitHub Actions & Bun script

---
+++ main.js
@@ -0,0 +1,42 @@
+import { $ } from 'bun';
+
+console.log('Fetching versions');
+const items =
+    await fetch('https://vivaldi.com/source/')
+        .then(response => response.text())
+        .then(html => html.matchAll(/<a href="(https:\/\/source\.vivaldi\.com\/vivaldi-source_([0-9.]+)\.tar.xz)".+?modified hidemobile">([^<]+)/g))
+        .then(matches => [...matches])
+        .then(matches => matches.map(([
+            , url
+            , version
+            , date
+        ]) => ({
+            url,
+            version,
+            date
+        })))
+        .then(items => items.toReversed());
+console.log(`Fetched ${items.length} versions`);
+for(const {
+    url,
+    version,
+    date
+} of items){
+    console.log(`Processing version ${version}`);
+    if(await $`git tag -l ${version}`.text()){
+        console.log(`Version ${version} already exists`);
+        continue;
+    }
+    console.log(`Downloading ${url}`);
+    await $`curl -L ${url} | tar xJ`;
+    console.log(`Staging version ${version}`);
+    await $`git add vivaldi-source`
+    console.log(`Committing version ${version}`);
+    await $`git commit -m ${version} --date ${date}`;
+    console.log(`Tagging version ${version}`);
+    await $`git tag -a ${version} -m ${version}`;
+    console.log(`Pushing version ${version}`);
+    await $`git push`;
+    console.log(`Version ${version} pushed`)
+}
+console.log('Done');
